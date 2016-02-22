c776f1eab3601db162e384ba99f237347bae4d37
conner <c9er99@gmail.com>
2016-02-21 21:50:48 -0800

    it will be the final prouduct

--- app/app.php
+++ app/app.php
@@ -1,12 +1,27 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/String.php";
+
+      session_start();
+      if(empty($_SESSION['input'])) {
+        $_SESSION['input'] = array();
+      }
+
     $app = new Silex\Application();
     $app-> register(new Silex\Provider\TwigServiceProvider(), array(
       'twig.path' =>__DIR__.'/../views'
     ));
+
     $app->get("/", function() use ($app) {
-      return $app['twig']->render('stringHome.html.twig'
-    }));
+      return $app['twig']->render('stringHome.html.twig');
+    });
+
+    $app->post("/stringMade", function() use ($app) {
+      $counts = new String;
+      $results = $counts->string_count($_POST['string'], $_POST['count']);
+
+      return $app['twig']->render('stringMade.html.twig', array('results' => $results));
+    });
+
       return $app;
- ?>
+ ?> 
